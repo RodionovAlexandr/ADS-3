f16ee9963b5784656e291ba604be3a9ee2c5bde8
RodionovAlexandr <99204870+RodionovAlexandr@users.noreply.github.com>
2022-03-27 19:00:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,13 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int how = 0;
-	int elem = 1;
-	for (int i = 0; i <= size; i++) {
-		elem = *(arr + i);
-		if (elem == value) {
-			how += 1;
-		}
-	}
-	return how;
-	return 0; // если ничего не найдено
+  int elem = 1;
+    for (int i = 0; i <= size; i++) {
+      elem = *(arr + i);
+      if (elem == value) {
+        how += 1;
+      }
+    }
+      return how;
+      return 0; // если ничего не найдено
 }
